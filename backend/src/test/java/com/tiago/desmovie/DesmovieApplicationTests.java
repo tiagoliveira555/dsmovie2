6913715dbb35f7a12e7020314585b24b3a39fd8c
tiagoliveira555 <tiagoliveira555@gmail.com>
2022-03-16 19:01:47 -0300

    Project create

---
+++ backend/src/test/java/com/tiago/desmovie/DesmovieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiago.desmovie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesmovieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
